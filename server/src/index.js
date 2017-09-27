946c6b3b011e0e20452b78aa1322f8e8e67b6815
Gwinyai Chakonda <gtchax@gmail.com>
2017-09-27 16:08:43 +0800

    Init express with babel config

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+//Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+//Routes
+
+
+app.listen(port, () => console.log(`
+============================
+Server is running on ${port}..
+=============================`));
+
+

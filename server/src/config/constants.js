30a93847911bcd227574e43df28424729aece507
Gwinyai Chakonda <gtchax@gmail.com>
2017-09-27 17:04:49 +0800

    Add and configure graphql server

--- server/src/config/constants.js
+++ server/src/config/constants.js
@@ -1,4 +1,5 @@
 export default {
     PORT: process.env.PORT || 4000,
-    DB_URL: "mongodb://localhost/tweet-dev"
+    DB_URL: "mongodb://localhost/tweet-dev",
+    GRAPHQL_PATH: '/graphql'
 }
--- server/src/index.js
+++ server/src/index.js
@@ -2,24 +2,40 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import { graphiqlExpress, graphqlExpress} from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import { createServer } from 'http';
+
+import constants from './config/constants';
 import './config/db';
+import typeDefs from './graphql/schema';
+import resolvers from './graphql/resolvers'; 
 
 const app = express();
-const port = process.env.PORT || 4000;
+
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
 
 //Middleware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('/graphiql', graphiqlExpress({
+    endpointURL: constants.GRAPHQL_PATH
+}));
+app.use(constants.GRAPHQL_PATH, graphqlExpress({
+    schema
+}))
 
+const graphqlServer = createServer(app);
 
-
-//Routes
-
-
-app.listen(port, () => console.log(`
+// Server
+graphqlServer.listen(constants.PORT, () => console.log(`
 ============================
-Server is running on ${port}..
+Server is running on ${constants.PORT}..
 =============================`));
 
 
